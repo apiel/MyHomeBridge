fe4423b129a9dd1e74ea7265515a17d3a525d2e3
Your Name <you@example.com>
2016-12-18 15:29:24 +0000

    remove dist-ts from repo

--- dist-ts/item/item.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-class ItemStatus {
-}
-exports.ItemStatus = ItemStatus;
-class Item {
-}
-exports.Item = Item;
--- dist-ts/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-require("babel-polyfill");
-let iterable = [10, 20, 30];
-console.log('a');
-for (let value of iterable) {
-    console.log(value);
-}
-console.log('b');
