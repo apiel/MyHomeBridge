b889dcc6ce88e30e3f84b9f0a62e18fb740949b5
alex <alexandre.piel@gmail.com>
2017-01-28 01:20:09 +0100

    mqttd service and refactoring

---
+++ app/lib/mqttd.service.ts
@@ -0,0 +1,85 @@
+
+// We should be able to choose between self hosted mqtt broker 
+// Or an external broker
+
+//import * as mqtt from 'mqtt'; // mqtt client if not use mqtt broker
+
+
+// we might have a main service and then some child service for  item, action...
+      
+
+// var client  = mqtt.connect('mqtt://test.mosquitto.org')
+ 
+// client.on('connect', function () {
+//   client.subscribe('presence')
+//   client.publish('presence', 'Hello mqtt')
+// })
+ 
+// client.on('message', function (topic, message) {
+//   // message is Buffer 
+//   console.log(message.toString())
+//   client.end()
+// })
+
+const mosca = require('mosca'); // mqtt broker
+
+export class Mqttd {
+    mqttd: any;
+    separator: string = '/';
+
+    constructor(port: number = 1883) {
+        this.mqttd = new mosca.Server({
+            port: port,
+            persistence: {
+                factory: mosca.persistence.Memory
+            }
+        });
+    }
+
+    publish(topic: string, payload: string, retain: boolean = true, qos: number = 0) {
+        this.mqttd.publish({
+            topic: topic,
+            payload: payload,
+            retain: retain,
+            qos: qos
+        }); 
+        return this;
+    }
+
+    ready(callback: Function) {
+        this.mqttd.on('ready', callback);
+        return this;
+    }
+
+    onPublished(callback: (packet: any, client: any) => any) {
+        this.mqttd.on('published', (packet: any, client: any) => { 
+            // Here we could force retain
+            if (client) { //  && client.id !== 'myhomebridge-server' --> for external mqtt server
+                callback(packet, client);
+            }
+        });
+        return this;
+    }
+}
+
+export class MqttdRoute { // not sure that's the bestName
+    constructor(private mqttd: Mqttd, private baseTopicLevel: string) {
+        this.baseTopicLevel = this.baseTopicLevel + this.mqttd.separator;
+    }
+
+    publish(topic: string, payload: string, retain: boolean = true, qos: number = 0) {
+        this.mqttd.publish(this.baseTopicLevel + topic,
+                            payload, retain, qos);
+        return this;
+    }
+
+    onPublished(callback: (packet: any, client: any) => any) {
+        this.mqttd.onPublished((packet: any, client: any) => { 
+            if (packet.topic.indexOf(this.baseTopicLevel) === 0) {
+                packet.topic = packet.topic.substring(this.baseTopicLevel.length);
+                callback(packet, client);
+            }
+        });
+        return this;
+    }
+}
--- app/myHomeBridge.ts
+++ app/myHomeBridge.ts
@@ -3,8 +3,21 @@ import ItemService from './item/item.service';
 import { Item, ItemStatus } from './item/item';
 import { Model, ModelObject } from './lib/model.helper';
 import Httpd from './lib/httpd.service';
+import { Mqttd, MqttdRoute } from './lib/mqttd.service';
 
-var mosca = require('mosca');
+import TriggerService  from './trigger/trigger.service';
+import { Trigger } from './trigger/trigger';
+
+import { Timer } from './timer/timer';
+import TimerService from './timer/timer.service';
+
+import ActionService  from './action/action.service';
+import { Action } from './action/action';
+
+import AlexaService  from './alexa/alexa.service';
+
+import DashboardService  from './dashboard/dashboard.service';
+import { DashboardCategory } from './dashboard/dashboard';
 
 const eventEmitter = new Events.EventEmitter();
 
@@ -12,6 +25,9 @@ let httpd = new Httpd();
 
 // We could find a way to load each module in a generic way, with dependency...
 
+// The model could be initialized in the service, or... ?
+// But since the model could change from file base to database, shared memrory...
+// We can't pass the path as parameter to the service
 
 let itemModel = new ModelObject<Item>("/../data/items.json");
 let itemService = new ItemService(itemModel, eventEmitter);
@@ -20,50 +36,53 @@ httpd.get('/item/status', ['id'], itemService.getStatus.bind(itemService));
 httpd.get('/item', ['id', 'status'], itemService.setStatusAsync.bind(itemService));
 httpd.get('/items', ['id', 'status'], itemService.allStatus.bind(itemService));
 
-
-import TriggerService  from './trigger/trigger.service';
-import { Trigger } from './trigger/trigger';
 let triggerModel = new ModelObject<Trigger>("/../data/triggers.json");
 let triggerService = new TriggerService(triggerModel);
 
-
-import { Timer } from './timer/timer';
-import TimerService from './timer/timer.service';
 let timerModel = new Model<Timer[]>("/../data/timers.json");
 let timerService = new TimerService(timerModel, itemService);
 timerService.init();
 
-
-import ActionService  from './action/action.service';
-import { Action } from './action/action';
 let actionModel = new ModelObject<Action[]>("/../data/actions.json");
 let actionService = new ActionService(actionModel, itemService, timerService);
 httpd.get('/action/definitions', [], actionService.definitions.bind(actionService));
 httpd.get('/action/:name', ['name'], actionService.call.bind(actionService)); // we should change it as item
 
-
-
-
-import AlexaService  from './alexa/alexa.service';
 let alexaModel = new ModelObject<Action>("/../data/alexa.json");
 let alexaService = new AlexaService(alexaModel, actionService);
 httpd.post('/alexa', alexaService.call.bind(alexaService));
 
-
-
-
-
-import DashboardService  from './dashboard/dashboard.service';
-import { DashboardCategory } from './dashboard/dashboard';
 let dashboardModel = new Model<DashboardCategory[]>("/../data/dashboard.json");
 let dashboardService = new DashboardService(dashboardModel, itemService, actionService);
 httpd.get('/dashboard/list', [], dashboardService.list.bind(dashboardService));
 
-
 httpd.serve();
+
+let mqttd = new Mqttd();
+let mqttdItem = new MqttdRoute(mqttd, 'item');
+let mqttdAction = new MqttdRoute(mqttd, 'action');
+
+mqttd.ready(() => {
+    try {
+        itemService.mapStatus(itemStatus => {
+                console.log(itemStatus);
+                mqttdItem.publish(itemStatus.id, itemStatus.status.toString());
+            });            
+    }
+    catch(error) {
+        console.log('Error on item setup: ', error);
+    }        
+});
+
+mqttdItem.onPublished((packet: any, client: any) => itemService.setStatus(packet.topic, packet.payload.toString()));
+mqttdAction.onPublished((packet: any, client: any) => this.actionService.call(packet.topic));
+eventEmitter.on('set/item/status', (itemStatus: ItemStatus) => mqttdItem.publish(itemStatus.id, itemStatus.status.toString()));
+
 
 
 // Mosca setup
+/*
+//var mosca = require('mosca');
 
 var mqttd = new mosca.Server({
   port: 1883,
@@ -78,22 +97,12 @@ mqttd.on('ready', () => {
         itemService.mapStatus(itemStatus => {
                 console.log(itemStatus);
                 mqttd.publish({
-                    topic: itemStatus.id,
+                    topic: 'item/' + itemStatus.id,
                     payload: itemStatus.status,
                     retain: true,
                     qos: 0
                 });
-            }); 
-        // this.itemService.allObservable().subscribe(
-        //     itemStatus => {
-        //         mqttd.publish({
-        //             topic: itemStatus.id,
-        //             payload: itemStatus.status,
-        //             retain: true,
-        //             qos: 0
-        //         });
-        //     }
-        // );            
+            });            
     }
     catch(error) {
         console.log('Error on item setup: ', error);
@@ -124,4 +133,4 @@ mqttd.on('published', (packet: any, client: any) => {
       this.actionService.call(packet.substring(6).topic);
   }
 });
-
+*/
